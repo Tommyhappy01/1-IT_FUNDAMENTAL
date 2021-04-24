1df2cb0367f4ced5799fbe0356483c702444ef57
Tommyhappy <81360106+Tommyhappy01@users.noreply.github.com>
2021-04-25 00:09:50 +0300

    Add files via upload

---
+++ python/try/chronic.py
@@ -0,0 +1,9 @@
+Age = input("Are you a cigarette addict older than 75 years old? ")
+Chronic = input("Do you have a severe chronic disease? ")
+İmmune =input("Is your immune system too weak? ")
+  
+if Age=="yes" or Chronic=="yes" or İmmune=="yes":
+    print("Sorry! You are in risky group")
+else:
+    print("Bravo! You are not in risky group")
+    
